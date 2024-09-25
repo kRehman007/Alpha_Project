092c69c4788f6dc4c534383af1dfa76f3b44068e
Mr.Kashan <kashisial2327@gmail.com>
2024-09-25 12:25:41 -0700

    Updation

--- src/Components/ContactUs/Contactus.jsx
+++ src/Components/ContactUs/Contactus.jsx
@@ -8,154 +8,240 @@ import {
   Input,
   Button,
 } from "@mui/material";
+import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import EastIcon from "@mui/icons-material/East";
+
 const Contactus = () => {
   const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    navigate("/querySuccess");
+  };
+
   return (
     <Container sx={{ marginTop: "60px" }}>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: { xs: "column", md: "row" },
-          gap: { xs: 4, sm: 8, md: 15 },
-        }}
-      >
+      <form onSubmit={handleSubmit(onSubmit)}>
         <Box
           sx={{
             display: "flex",
-            flexDirection: "column",
-            gap: 1,
-            width: { xs: "100%", md: "40%" },
-            order: { xs: 2, md: 1 },
+            flexDirection: { xs: "column", md: "row" },
+            gap: { xs: 4, sm: 8, md: 15 },
           }}
         >
-          <Typography
-            sx={{
-              fontWeight: "medium",
-              fontSize: "35px",
-            }}
-          >
-            Contact Us
-          </Typography>
-          <Typography
-            sx={{ fontSize: "16px", fontWeight: "medium", color: "#878C91" }}
-          >
-            We're Here to Help – Reach Out to Our Team for Any Inquiries or
-            Support Needs
-          </Typography>
           <Box
             sx={{
-              marginTop: "20px",
               display: "flex",
-              flexDirection: { xs: "column", sm: "row", md: "column" },
-              flexWrap: { sm: "wrap", md: "nowrap" },
-              alignItems: "flex-start",
-              gap: { xs: 3, sm: 7, md: 3 },
+              flexDirection: "column",
+              gap: 1,
+              width: { xs: "100%", md: "40%" },
+              order: { xs: 2, md: 1 },
             }}
           >
-            <FormControl sx={{ width: { xs: "100%", sm: "45%", md: "100%" } }}>
-              <InputLabel
-                htmlFor="email-input"
-                sx={{
-                  fontSize: "14px",
-                  color: "#878C91",
-                  marginLeft: "-12px",
-                }}
-              >
-                Email
-              </InputLabel>
-              <Input id="email-input" aria-describedby="email-helper-text" />
-            </FormControl>
-
-            <FormControl sx={{ width: { xs: "100%", sm: "45%", md: "100%" } }}>
-              <InputLabel
-                htmlFor="name-input"
-                sx={{
-                  fontSize: "14px",
-                  color: "#878C91",
-                  marginLeft: "-12px",
-                }}
-              >
-                Name
-              </InputLabel>
-              <Input id="name-input" aria-describedby="name-helper-text" />
-            </FormControl>
-
-            <FormControl sx={{ width: { xs: "100%", sm: "45%", md: "100%" } }}>
-              <InputLabel
-                htmlFor="phone-input"
-                sx={{
-                  fontSize: "14px",
-                  color: "#878C91",
-                  marginLeft: "-12px",
-                }}
-              >
-                Phone
-              </InputLabel>
-              <Input id="phone-input" aria-describedby="phone-helper-text" />
-            </FormControl>
-
-            <FormControl sx={{ width: { xs: "100%", sm: "45%", md: "100%" } }}>
-              <InputLabel
-                htmlFor="msg-input"
-                sx={{
-                  fontSize: "14px",
-                  color: "#878C91",
-                  marginLeft: "-12px",
-                }}
-              >
-                Message
-              </InputLabel>
-              <Input id="msg-input" aria-describedby="msg-helper-text" />
-            </FormControl>
-            <Box
-              onClick={() => navigate("/querySuccess")}
+            <Typography
               sx={{
-                display: "flex",
-                gap: 2,
-                alignItems: "center",
-                backgroundColor: "#000",
-                color: "#fff",
-                maxWidth: "min-content",
-                padding: "10px 20px",
-                borderRadius: "50px",
-                cursor: "pointer",
+                fontWeight: "medium",
+                fontSize: "35px",
               }}
             >
+              Contact Us
+            </Typography>
+            <Typography
+              sx={{ fontSize: "16px", fontWeight: "medium", color: "#878C91" }}
+            >
+              We're Here to Help – Reach Out to Our Team for Any Inquiries or
+              Support Needs
+            </Typography>
+            <Box
+              sx={{
+                marginTop: "20px",
+                display: "flex",
+                flexDirection: { xs: "column", sm: "row", md: "column" },
+                flexWrap: { sm: "wrap", md: "nowrap" },
+                alignItems: "flex-start",
+                gap: { xs: 3, sm: 7, md: 3 },
+              }}
+            >
+              <FormControl
+                sx={{ width: { xs: "100%", sm: "45%", md: "100%" } }}
+              >
+                <InputLabel
+                  htmlFor="email-input"
+                  sx={{
+                    fontSize: "14px",
+                    color: "#878C91",
+                    marginLeft: "-12px",
+                  }}
+                >
+                  Email
+                </InputLabel>
+                <Input
+                  type="email"
+                  id="email-input"
+                  {...register("email", { required: "Email is required" })}
+                  aria-describedby="email-helper-text"
+                />
+                {errors.email && (
+                  <span
+                    style={{
+                      textAlign: "center",
+                      fontSize: "12px",
+                      color: "red",
+                      marginTop: "7px",
+                    }}
+                  >
+                    {errors.email.message}
+                  </span>
+                )}
+              </FormControl>
+
+              <FormControl
+                sx={{ width: { xs: "100%", sm: "45%", md: "100%" } }}
+              >
+                <InputLabel
+                  htmlFor="name-input"
+                  sx={{
+                    fontSize: "14px",
+                    color: "#878C91",
+                    marginLeft: "-12px",
+                  }}
+                >
+                  Name
+                </InputLabel>
+                <Input
+                  type="text"
+                  id="name-input"
+                  aria-describedby="name-helper-text"
+                  {...register("name", { required: "Name is required" })}
+                />
+                {errors.name && (
+                  <span
+                    style={{
+                      textAlign: "center",
+                      fontSize: "12px",
+                      color: "red",
+                      marginTop: "7px",
+                    }}
+                  >
+                    {errors.name.message}
+                  </span>
+                )}
+              </FormControl>
+
+              <FormControl
+                sx={{ width: { xs: "100%", sm: "45%", md: "100%" } }}
+              >
+                <InputLabel
+                  htmlFor="phone-input"
+                  sx={{
+                    fontSize: "14px",
+                    color: "#878C91",
+                    marginLeft: "-12px",
+                  }}
+                >
+                  Phone
+                </InputLabel>
+                <Input
+                  {...register("phone", {
+                    required: "Phone number is required",
+                  })}
+                  type="tel"
+                  id="phone-input"
+                  aria-describedby="phone-helper-text"
+                />
+                {errors.phone && (
+                  <span
+                    style={{
+                      textAlign: "center",
+                      fontSize: "12px",
+                      color: "red",
+                      marginTop: "7px",
+                    }}
+                  >
+                    {errors.phone.message}
+                  </span>
+                )}
+              </FormControl>
+
+              <FormControl
+                sx={{ width: { xs: "100%", sm: "45%", md: "100%" } }}
+              >
+                <InputLabel
+                  htmlFor="msg-input"
+                  sx={{
+                    fontSize: "14px",
+                    color: "#878C91",
+                    marginLeft: "-12px",
+                  }}
+                >
+                  Message
+                </InputLabel>
+                <Input
+                  {...register("message", {
+                    required: "Enter a short Message here...!",
+                  })}
+                  type="text"
+                  id="msg-input"
+                  aria-describedby="msg-helper-text"
+                />
+                {errors.message && (
+                  <span
+                    style={{
+                      textAlign: "center",
+                      fontSize: "12px",
+                      color: "red",
+                      marginTop: "7px",
+                    }}
+                  >
+                    {errors.message.message}
+                  </span>
+                )}
+              </FormControl>
               <button
+                type="submit"
                 style={{
-                  backgroundColor: "transparent",
+                  backgroundColor: "#000",
                   border: "none",
                   color: "#fff",
                   cursor: "pointer",
+                  display: "flex",
+
+                  alignItems: "center",
+                  padding: "5px 15px",
+                  borderRadius: "50px",
                 }}
               >
                 Send
+                <EastIcon sx={{ color: "#fff", marginLeft: "15px" }} />
               </button>
-              <EastIcon sx={{ color: "#fff" }} />
             </Box>
           </Box>
+          <Box
+            sx={{
+              flexGrow: 1,
+              borderRadius: "10px",
+              height: { xs: "300px", sm: "350px", md: "auto" },
+              marginTop: { md: "20px" },
+              order: { xs: 1, md: 2 },
+            }}
+          >
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3378.95272396428!2d72.67854097472376!3d32.124579017044034!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3921766c3b825095%3A0x8881f2f6014a5725!2sMari%2C%20Sargodha%2C%20Punjab%2C%20Pakistan!5e0!3m2!1sen!2s!4v1727275697187!5m2!1sen!2s"
+              width="100%"
+              height="100%"
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </Box>
         </Box>
-        <Box
-          sx={{
-            flexGrow: 1,
-            borderRadius: "10px",
-            height: { xs: "300px", sm: "350px", md: "auto" },
-            marginTop: { md: "20px" },
-            order: { xs: 1, md: 2 },
-          }}
-        >
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3378.95272396428!2d72.67854097472376!3d32.124579017044034!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3921766c3b825095%3A0x8881f2f6014a5725!2sMari%2C%20Sargodha%2C%20Punjab%2C%20Pakistan!5e0!3m2!1sen!2s!4v1727275697187!5m2!1sen!2s"
-            width="100%"
-            height="100%"
-            allowFullScreen=""
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
-        </Box>
-      </Box>
+      </form>
     </Container>
   );
 };
--- src/Components/GetQuote/GetQuote.jsx
+++ src/Components/GetQuote/GetQuote.jsx
@@ -13,7 +13,14 @@ import CloudDownloadIcon from "@mui/icons-material/CloudDownload";
 import { useNavigate } from "react-router-dom";
 import React, { useRef, useState } from "react";
 import EastIcon from "@mui/icons-material/East";
-const ContactUs = () => {
+import { useForm } from "react-hook-form";
+
+const getQuote = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const navigate = useNavigate();
   const fileRef = useRef(null);
   const [country, setCountry] = useState("");
@@ -41,6 +48,12 @@ const ContactUs = () => {
     { code: "IN", name: "India" },
     // Add more countries as needed
   ];
+
+  const onSubmit = (data) => {
+    console.log(data);
+    navigate("/querySuccess");
+  };
+
   return (
     <Container
       sx={{
@@ -48,219 +61,281 @@ const ContactUs = () => {
         marginBottom: "40px",
       }}
     >
-      <Typography
-        sx={{ fontWeight: "medium", fontSize: "35px", marginBottom: "30px" }}
-      >
-        Get a Quote
-      </Typography>
-      <Box
-        sx={{
-          display: "flex",
-          flexWrap: "wrap",
-          columnGap: 14,
-          rowGap: 10,
-          background: "lightgray",
-          borderRadius: "10px",
-          padding: "30px 40px",
-        }}
-      >
-        <FormControl
-          sx={{ width: { xs: "100%", sm: "40%", md: "43%", lg: "44%" } }}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Typography
+          sx={{ fontWeight: "medium", fontSize: "35px", marginBottom: "30px" }}
         >
-          <InputLabel
-            htmlFor="name-input"
-            sx={{
-              fontSize: "14px",
-              color: "#878C91",
-              marginLeft: "-12px",
-            }}
-          >
-            Name
-          </InputLabel>
-          <Input
-            id="name-input"
-            aria-describedby="name-helper-text"
-            sx={{
-              width: "100%",
-            }}
-          />
-        </FormControl>
-        <FormControl
-          sx={{ width: { xs: "100%", sm: "40%", md: "43%", lg: "44%" } }}
-        >
-          <InputLabel
-            htmlFor="email-input"
-            sx={{
-              fontSize: "14px",
-              color: "#878C91",
-              marginLeft: "-12px",
-            }}
-          >
-            Email Address
-          </InputLabel>
-          <Input
-            id="email-input"
-            aria-describedby="email-helper-text"
-            sx={{
-              width: "100%",
-            }}
-          />
-        </FormControl>
-        <FormControl
-          sx={{ width: { xs: "100%", sm: "40%", md: "43%", lg: "44%" } }}
-        >
-          <InputLabel
-            htmlFor="telephone-input"
-            sx={{
-              fontSize: "14px",
-              color: "#878C91",
-              marginLeft: "-12px",
-            }}
-          >
-            Phone No
-          </InputLabel>
-          <Input
-            id="telephone-input"
-            aria-describedby="telephone-helper-text"
-            sx={{
-              width: "100%",
-            }}
-          />
-        </FormControl>
-        <FormControl
-          sx={{ width: { xs: "100%", sm: "41%", md: "43%", lg: "44%" } }}
-        >
-          <InputLabel
-            id="country-select-label"
-            sx={{
-              fontSize: "14px",
-              color: "#878C91",
-            }}
-          >
-            Country
-          </InputLabel>
-          <Select
-            labelId="country-select-label"
-            id="country-select"
-            value={country}
-            onChange={handleCountryChange}
-            label="Country"
-          >
-            {countries.map((country) => (
-              <MenuItem key={country.code} value={country.code}>
-                {country.name}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <FormControl
-          sx={{ width: { xs: "100%", sm: "40%", md: "43%", lg: "44%" } }}
-        >
-          <InputLabel
-            htmlFor="project-input"
-            sx={{
-              fontSize: "14px",
-              color: "#878C91",
-              marginLeft: "-12px",
-            }}
-          >
-            ProjectType
-          </InputLabel>
-          <Input
-            id="project-input"
-            aria-describedby="project-helper-text"
-            sx={{
-              width: "100%",
-            }}
-          />
-        </FormControl>
-        <FormControl
-          sx={{ width: { xs: "100%", sm: "40%", md: "43%", lg: "44%" } }}
-        >
-          <InputLabel
-            htmlFor="budget-input"
-            sx={{
-              fontSize: "14px",
-              color: "#878C91",
-              marginLeft: "-12px",
-            }}
-          >
-            Estimated Budget
-          </InputLabel>
-          <Input
-            id="budget-input"
-            aria-describedby="budget-helper-text"
-            sx={{
-              width: "100%",
-            }}
-          />
-        </FormControl>
+          Get a Quote
+        </Typography>
         <Box
           sx={{
-            width: "100%",
-            height: "250px",
-            border: "1px dotted black",
-            borderRadius: "10px",
             display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
+            flexWrap: "wrap",
+            columnGap: 14,
+            rowGap: 10,
+            background: "lightgray",
+            borderRadius: "10px",
+            padding: "30px 40px",
           }}
         >
-          <input
-            type="file"
-            ref={fileRef}
-            style={{ display: "none" }}
-            onChange={handleFile}
-          />
+          <FormControl
+            sx={{ width: { xs: "100%", sm: "40%", md: "43%", lg: "44%" } }}
+          >
+            <InputLabel
+              htmlFor="name-input"
+              sx={{
+                fontSize: "14px",
+                color: "#878C91",
+                marginLeft: "-12px",
+              }}
+            >
+              Name
+            </InputLabel>
+            <Input
+              {...register("name", { required: "Name is required" })}
+              type="text"
+              id="name-input"
+              aria-describedby="name-helper-text"
+              sx={{
+                width: "100%",
+              }}
+            />
+            {errors.name && (
+              <span
+                style={{
+                  textAlign: "center",
+                  fontSize: "12px",
+                  color: "red",
+                  marginTop: "7px",
+                }}
+              >
+                {errors.name.message}
+              </span>
+            )}
+          </FormControl>
+          <FormControl
+            sx={{ width: { xs: "100%", sm: "40%", md: "43%", lg: "44%" } }}
+          >
+            <InputLabel
+              htmlFor="email-input"
+              sx={{
+                fontSize: "14px",
+                color: "#878C91",
+                marginLeft: "-12px",
+              }}
+            >
+              Email Address
+            </InputLabel>
+            <Input
+              {...register("email", { required: "Email is required" })}
+              type="email"
+              id="email-input"
+              aria-describedby="email-helper-text"
+              sx={{
+                width: "100%",
+              }}
+            />
+            {errors.email && (
+              <span
+                style={{
+                  textAlign: "center",
+                  fontSize: "12px",
+                  color: "red",
+                  marginTop: "7px",
+                }}
+              >
+                {errors.email.message}
+              </span>
+            )}
+          </FormControl>
+          <FormControl
+            sx={{ width: { xs: "100%", sm: "40%", md: "43%", lg: "44%" } }}
+          >
+            <InputLabel
+              htmlFor="telephone-input"
+              sx={{
+                fontSize: "14px",
+                color: "#878C91",
+                marginLeft: "-12px",
+              }}
+            >
+              Phone No
+            </InputLabel>
+            <Input
+              {...register("phone", { required: "Phone is required" })}
+              type="tel"
+              id="telephone-input"
+              aria-describedby="telephone-helper-text"
+              sx={{
+                width: "100%",
+              }}
+            />
+            {errors.phone && (
+              <span
+                style={{
+                  textAlign: "center",
+                  fontSize: "12px",
+                  color: "red",
+                  marginTop: "7px",
+                }}
+              >
+                {errors.phone.message}
+              </span>
+            )}
+          </FormControl>
+          <FormControl
+            sx={{ width: { xs: "100%", sm: "41%", md: "43%", lg: "44%" } }}
+          >
+            <InputLabel
+              id="country-select-label"
+              sx={{
+                fontSize: "14px",
+                color: "#878C91",
+              }}
+            >
+              Country
+            </InputLabel>
+            <Select
+              {...register("country")}
+              labelId="country-select-label"
+              id="country-select"
+              value={country}
+              onChange={handleCountryChange}
+              label="Country"
+            >
+              {countries.map((country) => (
+                <MenuItem key={country.code} value={country.code}>
+                  {country.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl
+            sx={{ width: { xs: "100%", sm: "40%", md: "43%", lg: "44%" } }}
+          >
+            <InputLabel
+              htmlFor="project-input"
+              sx={{
+                fontSize: "14px",
+                color: "#878C91",
+                marginLeft: "-12px",
+              }}
+            >
+              ProjectType
+            </InputLabel>
+            <Input
+              {...register("project", { required: "Enter Project Type" })}
+              type="text"
+              id="project-input"
+              aria-describedby="project-helper-text"
+              sx={{
+                width: "100%",
+              }}
+            />
+            {errors.project && (
+              <span
+                style={{
+                  textAlign: "center",
+                  fontSize: "12px",
+                  color: "red",
+                  marginTop: "7px",
+                }}
+              >
+                {errors.project.message}
+              </span>
+            )}
+          </FormControl>
+          <FormControl
+            sx={{ width: { xs: "100%", sm: "40%", md: "43%", lg: "44%" } }}
+          >
+            <InputLabel
+              htmlFor="budget-input"
+              sx={{
+                fontSize: "14px",
+                color: "#878C91",
+                marginLeft: "-12px",
+              }}
+            >
+              Estimated Budget
+            </InputLabel>
+            <Input
+              {...register("budget", { required: "Enter your Budget" })}
+              type="text"
+              id="budget-input"
+              aria-describedby="budget-helper-text"
+              sx={{
+                width: "100%",
+              }}
+            />
+            {errors.budget && (
+              <span
+                style={{
+                  textAlign: "center",
+                  fontSize: "12px",
+                  color: "red",
+                  marginTop: "7px",
+                }}
+              >
+                {errors.budget.message}
+              </span>
+            )}
+          </FormControl>
           <Box
             sx={{
+              width: "100%",
+              height: "250px",
+              border: "1px dotted black",
+              borderRadius: "10px",
               display: "flex",
-              flexDirection: "column",
+              justifyContent: "center",
               alignItems: "center",
             }}
           >
-            <CloudDownloadIcon
-              onClick={() => fileRef.current.click()}
-              sx={{ fontSize: "50px", cursor: "pointer" }}
+            <input
+              type="file"
+              ref={fileRef}
+              style={{ display: "none" }}
+              onChange={handleFile}
             />
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <CloudDownloadIcon
+                onClick={() => fileRef.current.click()}
+                sx={{ fontSize: "50px", cursor: "pointer" }}
+              />
 
-            <Typography sx={{ color: "#878C91", fontSize: "12px" }}>
-              Drag/Download file
-            </Typography>
+              <Typography sx={{ color: "#878C91", fontSize: "12px" }}>
+                Drag/Download file
+              </Typography>
+            </Box>
           </Box>
         </Box>
-      </Box>
 
-      <Box
-        onClick={() => navigate("/SuccessMsg")}
-        sx={{
-          display: "flex",
-          gap: 2,
-          alignItems: "center",
-          backgroundColor: "#000",
-          color: "#fff",
-          widht: "min-content",
-          float: "right",
-          padding: "10px 20px",
-          margin: "40px 0",
-          borderRadius: "50px",
-          cursor: "pointer",
-        }}
-      >
         <button
+          type="submit"
           style={{
-            backgroundColor: "transparent",
+            backgroundColor: "#000",
             border: "none",
             color: "#fff",
             cursor: "pointer",
+            display: "flex",
+            marginTop: "10px",
+            alignItems: "center",
+            padding: "5px 15px",
+            borderRadius: "50px",
           }}
         >
           Send
+          <EastIcon sx={{ color: "#fff", marginLeft: "15px" }} />
         </button>
-        <EastIcon sx={{ color: "#fff" }} />
-      </Box>
+      </form>
     </Container>
   );
 };
 
-export default ContactUs;
+export default getQuote;
